0720ea5df89169d15abd215c43241021f17f3b67
bkotskyy <bohdan.kotskyy@bambora.com>
2023-03-09 10:56:55 +0200

    wifi reconection and refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -16,25 +16,23 @@ struct Config {
   String password;
   String chatId;
   unsigned int tag;
-  unsigned int portionWeightPerDay;
+  unsigned int portion;
   unsigned int firstMealTime;
   unsigned int lastMealTime;
-  unsigned int numberOfMealsPerDay;
+  unsigned int numberOfMeals;
 
   String toString() {
     return "{\n\t\"ssid\": \"" + ssid + "\"\n" +
     "\t\"password\": \"" + password + "\"\n" + 
     "\t\"chatId\": \"" + chatId + "\"\n" + 
     "\t\"tag\": " + String(tag) + "\n" + 
-    "\t\"portionWeightPerDay\": " + String(portionWeightPerDay) + "\n" + 
+    "\t\"portion\": " + String(portion) + "\n" + 
     "\t\"firstMealTime\": " + String(firstMealTime) + "\n" + 
     "\t\"lastMealTime\": " + String(lastMealTime) + "\n" + 
-    "\t\"numberOfMealsPerDay\": " + String(numberOfMealsPerDay) + "\n}";
+    "\t\"numberOfMeals\": " + String(numberOfMeals) + "\n}";
   }
 };
 Config config;
-
-
 
 // Initialize Telegram BOT
 #define BOTtoken "5933476596:AAG-mZ1tfNy0boHKzrOdjFmFLCckUIfEClc"
@@ -52,14 +50,13 @@ String ssidList = "";
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-
 void initSDCard(){
   if(!SD.begin()){
     Serial.println("Card Mount Failed");
     return;
   }
-  uint8_t cardType = SD.cardType();
 
+  uint8_t cardType = SD.cardType();
   if(cardType == CARD_NONE){
     Serial.println("No SD card attached");
     return;
@@ -88,23 +85,24 @@ void getConfig() {
   }
 
   // Parse the JSON data
-  StaticJsonDocument<512> doc;
-  DeserializationError error = deserializeJson(doc, configFile);
+  StaticJsonDocument<512> configJson;
+  DeserializationError error = deserializeJson(configJson, configFile);
+  configFile.close();
   if (error) {
     Serial.print("Failed to parse JSON: ");
     Serial.println(error.c_str());
     return;
   }
 
-  // Extract the values from the JSON data
-  config.ssid = doc["ssid"] ? doc["ssid"].as<String>() : "";
-  config.password = doc["password"] ? doc["password"].as<String>() : "";
-  config.chatId = doc["chatId"] ? doc["chatId"].as<String>() : "";
-  config.tag = doc["tag"] ? doc["tag"] : 0;
-  config.portionWeightPerDay = doc["portionWeightPerDay"] ? doc["portionWeightPerDay"] : 0;
-  config.firstMealTime = doc["firstMealTime"] ? doc["firstMealTime"] : 0;
-  config.lastMealTime = doc["lastMealTime"] ? doc["lastMealTime"] : 0;
-  config.numberOfMealsPerDay = doc["numberOfMealsPerDay"] ? doc["numberOfMealsPerDay"] : 0;
+  // Extract the values from the JSON
+  config.ssid = configJson["ssid"] ? configJson["ssid"].as<String>() : "";
+  config.password = configJson["password"] ? configJson["password"].as<String>() : "";
+  config.chatId = configJson["chatId"] ? configJson["chatId"].as<String>() : "";
+  config.tag = configJson["tag"] ? configJson["tag"] : 0;
+  config.portion = configJson["portion"] ? configJson["portion"] : 0;
+  config.firstMealTime = configJson["firstMealTime"] ? configJson["firstMealTime"] : 0;
+  config.lastMealTime = configJson["lastMealTime"] ? configJson["lastMealTime"] : 0;
+  config.numberOfMeals = configJson["numberOfMeals"] ? configJson["numberOfMeals"] : 0;
 }
 
 void saveConfig() {
@@ -115,16 +113,10 @@ void saveConfig() {
     return;
   }
 
-  // Read the contents of the JSON file
-  String jsonContents = "";
-  while (configFile.available()) {
-    jsonContents += char(configFile.read());
-  }
-  configFile.close();
-
   // Parse the JSON data
-  DynamicJsonDocument doc(1024);
-  DeserializationError error = deserializeJson(doc, jsonContents);
+  StaticJsonDocument<512> configJson;
+  DeserializationError error = deserializeJson(configJson, configFile);
+  configFile.close();
   if (error) {
     Serial.print("Failed to parse JSON: ");
     Serial.println(error.c_str());
@@ -132,12 +124,18 @@ void saveConfig() {
   }
 
   // Update the values in the JSON data
-  doc["ssid"] = config.ssid;
-  doc["password"] = config.password;
+  configJson["ssid"] = config.ssid;
+  configJson["password"] = config.password;
+  configJson["chatId"] = config.chatId;
+  configJson["tag"] = config.tag;
+  configJson["portion"] = config.portion;
+  configJson["firstMealTime"] = config.firstMealTime;
+  configJson["lastMealTime"] = config.lastMealTime;
+  configJson["numberOfMeals"] = config.numberOfMeals;
 
   // Serialize the JSON data to a string
   String jsonString;
-  serializeJson(doc, jsonString);
+  serializeJson(configJson, jsonString);
 
   // Open the file in "write" mode and overwrite the existing data
   configFile = SD.open("/config.json", FILE_WRITE);
@@ -151,15 +149,41 @@ void saveConfig() {
   configFile.close();
   
   Serial.println("JSON data updated!");
-//  delay(5000); // Wait for 5 seconds before updating the next field
-  
+  // delay(5000); // Wait for 5 seconds before updating the next field
+}
+
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("Connected to AP successfully!");
+}
+
+void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("Disconnected from WiFi access point");
+  Serial.print("WiFi lost connection. Reason: ");
+  Serial.println(info.wifi_sta_disconnected.reason);
+  // Serial.println("Trying to Reconnect");
+  // WiFi.begin(config.ssid.c_str(), config.password.c_str());
 }
 
 bool initWiFi() {
   if(config.ssid=="" || config.password==""){
-    Serial.println("Undefined SSID or password.");
+    Serial.println(F("WiFi credentials not found."));
     return false;
   }
+
+  // delete old config
+  // WiFi.disconnect(true);
+
+  // delay(1000);
+
+  WiFi.onEvent(WiFiStationConnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_CONNECTED);
+  WiFi.onEvent(WiFiGotIP, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_GOT_IP);
+  // WiFi.onEvent(WiFiStationDisconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(config.ssid.c_str(), config.password.c_str());
@@ -229,6 +253,64 @@ void scanWiFiNetworks() {
   }
 }
 
+void initWifiManager() {
+  Serial.println("Setting WiFi Manager");
+  scanWiFiNetworks();
+  WiFi.softAP("PET_FEEDER", NULL);
+    
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("WM IP address: ");
+  Serial.println(IP);
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SD, "/wifimanager.html", "text/html");
+  });
+    
+  server.on("/get_ssid_list", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "application/json", "[" + ssidList + "]");
+  });
+
+  server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+    int params = request->params();
+    for(int i=0;i<params;i++){
+      AsyncWebParameter* p = request->getParam(i);
+      if(p->isPost()){
+        // HTTP POST ssid value
+        if (p->name() == "ssid") {
+          config.ssid = p->value().c_str();
+          Serial.print("SSID set to: ");
+          Serial.println(config.ssid);
+        }
+        // HTTP POST pass value
+        if (p->name() == "password") {
+          config.password = p->value().c_str();
+          Serial.print("Password set to: ");
+          Serial.println(config.password);
+        }
+      }
+    }
+
+    saveConfig();
+    
+    request->send(200, "text/plain", "Done. ESP will restart and you can proceed with telegram");
+    delay(3000);
+    ESP.restart();
+  });
+
+  server.serveStatic("/", SD, "/");
+  server.begin();
+}
+
+void initWebServer() {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SD, "/index.html", "text/html");
+  });
+  
+  server.serveStatic("/", SD, "/");
+  server.begin();
+}
+
 void setup() {
   Serial.begin(115200);
   
@@ -236,63 +318,25 @@ void setup() {
   getConfig();
 
   if (initWiFi()) {
-    
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send(SD, "/index.html", "text/html");
-    });
-  
-    server.serveStatic("/", SD, "/");
-    server.begin();
+    initWebServer();
   } else {
-    Serial.println("Setting WiFi Manager");
-    scanWiFiNetworks();
-    WiFi.softAP("PET-FEEDER-WIFI-MANAGER", NULL);
-    
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP); 
-
-    // Web Server Root URL
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send(SD, "/wifimanager.html", "text/html");
-    });
-    
-    server.serveStatic("/", SD, "/");
-
-    server.on("/get_ssid_list", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send(200, "application/json", "[" + ssidList + "]");
-    });
-
-    server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
-      int params = request->params();
-      for(int i=0;i<params;i++){
-        AsyncWebParameter* p = request->getParam(i);
-        if(p->isPost()){
-          // HTTP POST ssid value
-          if (p->name() == "ssid") {
-            config.ssid = p->value().c_str();
-            Serial.print("SSID set to: ");
-            Serial.println(config.ssid);
-          }
-          // HTTP POST pass value
-          if (p->name() == "password") {
-            config.password = p->value().c_str();
-            Serial.print("Password set to: ");
-            Serial.println(config.password);
-          }
-        }
-      }
-      saveConfig();
-      request->send(200, "text/plain", "Done. ESP will restart and you can proceed with telegram");
-      delay(3000);
-      ESP.restart();
-    });
-    server.begin();
-    
+    initWifiManager();
   }
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
+  // if WiFi is down, try reconnecting every CHECK_WIFI_TIME seconds
+  if ((WiFi.getMode() == WIFI_STA) && (WiFi.status() != WL_CONNECTED)) {
+    if (currentMillis - previousMillis >=interval) {
+      Serial.print(millis());
+      Serial.println("Reconnecting to WiFi...");
+      WiFi.disconnect();
+      WiFi.reconnect();
+      previousMillis = currentMillis;
+    }
+  }
+
   if (millis() > lastTimeBotRan + botRequestDelay) {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
